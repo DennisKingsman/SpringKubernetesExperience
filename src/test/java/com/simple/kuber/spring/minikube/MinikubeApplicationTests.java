135fe19599efffc69d6034ea13e2569c5a75ae62
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-25 20:34:11 +0400

    init

---
+++ src/test/java/com/simple/kuber/spring/minikube/MinikubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.kuber.spring.minikube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinikubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
